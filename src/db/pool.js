f2f463301aec2a0789ebb1b5c97e14e481c304ba
luketucich <luketucich@gmail.com>
2025-03-27 21:37:00 -0400

    fix: moved .env into src folder.

--- src/db/pool.js
+++ src/db/pool.js
@@ -1,6 +1,12 @@
-require("dotenv").config();
 const { Pool } = require("pg");
 
-module.exports = new Pool({
+const pool = new Pool({
   connectionString: process.env.PG_URL,
 });
+
+pool
+  .query("SELECT current_database();")
+  .then((res) => console.log("Connected to DB:", res.rows[0].current_database))
+  .catch((err) => console.error("DB Connection Error:", err));
+
+module.exports = pool;
